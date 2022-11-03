84da72fdf69e164d5a0c2c8e5786cd79e7dee0d4
Evandro <34420396+ev4ndropc@users.noreply.github.com>
2022-11-03 19:52:05 -0300

    Update style-gabivoliveiraa.js

--- style-gabivoliveiraa.js
+++ style-gabivoliveiraa.js
@@ -1,3 +1,23 @@
-$('article').toArray().forEach(article=> {
-  $(article).addClass('animate__animated animate__bounce')
-})
+		$('.entry-title').toArray().forEach((title, i)=> {
+			setTimeout(() => {
+				$(title).addClass('animate__animated animate__fadeInDown')
+			}, i*50)
+		})
+		
+		$('.walkerpress-post-thumbnails img').toArray().forEach((img, i)=> {
+			setTimeout(() => {
+				$(img).addClass('animate__animated animate__fadeIn')
+			}, i*50)
+		})
+		
+		$('.entry-content p').toArray().forEach((p, i)=> {
+			setTimeout(() => {
+				$(p).addClass('animate__animated animate__fadeInUp')
+			}, i*50)
+		})
+		
+		$('.post-thumbnail img').toArray().forEach((img, i)=> {
+			setTimeout(() => {
+				$(img).addClass('animate__animated animate__fadeIn')
+			}, i*50)
+		})
